b84a06473c4d8e764842562f3d8e4b9e0a365f08
Dmitri Foka <d.foka@avantajprim.com>
2016-04-21 00:19:03 +0300

    Домашнее заданее №4 (Тест создания контакта)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class UserData {
+  private final String firstname;
+  private final String lastname;
+  private final String company;
+  private final String address;
+  private final String telephone;
+
+  public UserData(String firstname, String lastname, String company, String address, String telephone) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.company = company;
+    this.address = address;
+    this.telephone = telephone;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getTelephone() {
+    return telephone;
+  }
+}
