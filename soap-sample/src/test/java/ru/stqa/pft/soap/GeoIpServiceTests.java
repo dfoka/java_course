e49416bf518b9966cfbb6f442c77e24fa1fad348
Dmitri Foka <d.foka@avantajprim.com>
2016-06-07 21:36:24 +0300

    Добавлен пример работы SOAP api
    test GeoIP

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+  @Test
+  public void testMyIp(){
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("217.26.173.163");
+    assertEquals(geoIP.getCountryCode(), "MDA");
+  }
+}
